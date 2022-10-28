1f2e4bc0b7ff3fa8f343f87afc0dde546901d4b2
Vasil Manavski <Vasil.Manavski@netcetera.com>
2022-10-29 00:10:09 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,5 @@
+<script>
+  const name =" MEIL LISAL"
+</script>
+
+<div class="text-blue-700">Hello {name}</div>
---
+++ svelte.config.js
@@ -0,0 +1,16 @@
+import sveltePreprocess from 'svelte-preprocess'
+import preprocess from "svelte-preprocess";
+
+export default {
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: sveltePreprocess()
+}
+
+const config = {
+  preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
+}
