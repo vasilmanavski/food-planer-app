597e7125eb20a24c6376010e96dcc9aa70bc782d
Vasil Manavski <Vasil.Manavski@netcetera.com>
2022-10-30 02:13:01 +0100

    add what needed to be added #8

--- src/components/MainBody.svelte
+++ src/components/MainBody.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import IngredientForm from './IngredientForm.svelte';
   import {derivedIngredient} from '../store';
+  import Header from './Header.svelte';
   const apiURL = "https://api.edamam.com/api/recipes/v2?type=public";
   const apiKey = "4cdaa1065747c8199fb12f63cba9224f";
   const apiId = "1b75121c";
@@ -25,7 +26,9 @@
 </script>
 
 
-
+<Header />
 <IngredientForm />
-<button style="margin-left: 25%" on:click="{fetchRecipes}" type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+<button style="margin-top: 1%; margin-left: 25%" on:click="{fetchRecipes}" type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+
+
 <svelte:component this="{Recipe}" recipes={result}/>
--- src/components/Recipe.svelte
+++ src/components/Recipe.svelte
@@ -4,7 +4,7 @@
 
 </script>
 
-<div class="flex flex-wrap m-10">
+<div class="flex flex-wrap m-20">
   {#each recipes.hits as {recipe}}
     <div class="max-w-md rounded overflow-hidden shadow-lg grow sm-4">
       <img class="max-w-full" src="{recipe['image']}" alt="Sunset in the mountains">
@@ -14,8 +14,19 @@
           <a href="{recipe['url']}">Click to see full recipe</a>
         </p>
         <p class="text-gray-700 text-base">
-          Calories {recipe['calories']}
+          Calories {Math.round(recipe['calories'])}
         </p>
+        <hr/>
+        <p class="text-gray-700 text-base">
+         Carbs {Math.round(recipe['totalNutrients']['CHOCDF']['quantity'])}
+        </p>
+        <p class="text-gray-700 text-base">
+          Proteins {Math.round(recipe['totalNutrients']['PROCNT']['quantity'])}
+        </p>
+        <p class="text-gray-700 text-base">
+          Fat {Math.round(recipe['totalNutrients']['FAT']['quantity'])}
+        </p>
+
       </div>
       <div class="px-6 pt-4 pb-2">
         {#if recipe['cuisineType'] === ""}
@@ -29,7 +40,6 @@
         {#if recipe['dietLabels'] === ""}
           <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{recipe['dietLabels']}</span>
         {/if}
-
       </div>
     </div>
   {/each}
